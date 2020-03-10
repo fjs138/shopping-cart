caac0355f2ae15780e2046d374ae3a6dc258a599
Frank Santaguida <fjs138@gmail.com>
2020-03-10 00:37:22 -0400

    fixed reset, decrement

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,48 +4,26 @@ import Counter from './counter';
 class Counters extends Component {
     // props include data that we give to a component, read only
     // state includes data that is local or private to that component
-    state = {
-        counters: [
-            { id: 1, value: 0},
-            { id: 2, value: 0},
-            { id: 3, value: 0},
-            { id: 4, value: 0}
 
-        ]
-    };
-
-    handleIncrement = counter => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = {...counter};
-    counters[index].value++;
-    this.setState({counters});
-    console.log(this.state.counters[0]);
-};
-
-    handleReset = () => {
-    };
-
-    handleDelete = (counterId) => {
-        const counters = this.state.counters.filter(c => c.id !== counterId);this.setState({counters});
-    };
 
     render() {
         return (
         <div>
 
             <button
-                onClick={this.handleReset}
+                onClick={this.props.onReset}
                 className='btn btn-primary btn-sm m-2'>
             Reset
         </button>
 
-            { this.state.counters.map(counter => (
+            { this.props.counters.map(counter => (
                 <Counter
                     key={counter.id}
-                    onDelete={this.handleDelete}
-                    onIncrement={this.handleIncrement}
-                    counter={counter}/>
+                    onDelete={this.props.onDelete}
+                    onIncrement={this.props.onIncrement}
+                    onDecrement={this.props.onDecrement}
+                    counter={counter}
+                />
             ))}
         </div>
         );
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,14 @@ class NavBar extends Component {
     //state = {}
     render() {
         return (<nav className="navbar navbar-light bg-light">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <a className="navbar-brand" href="#">
+                Navbar <span className='badge badge-pill badge-secondary'>
+                {this.props.totalCounters}
+
+            </span>
+
+            </a>
+
         </nav>)
     }
 }
